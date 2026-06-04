32dedebfc0c8abeff3d9a4b0f91762e176269699
Xu Shaohua <shaohua@biofan.org>
2026-06-04 10:02:16 +0800

    icons: Remove inflections from build dependencies

--- crates/dioxus-icons-ionic/build.rs
+++ crates/dioxus-icons-ionic/build.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Copyright (c) 2026 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by Lesser General Public License
 // that can be found in the LICENSE file.
 
